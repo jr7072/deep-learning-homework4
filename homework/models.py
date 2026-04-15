563b07e0485b632003ebca6a3607290fb9d3a415
Juan Rosales <juanrosales@Juans-MacBook-Pro.local>
2026-04-15 00:21:48 -0500

    reduced network size

--- homework/models.py
+++ homework/models.py
@@ -120,7 +120,7 @@ class MLPPlanner(nn.Module):
         self,
         n_track: int = 10,
         n_waypoints: int = 3,
-        n_layers: int=10,
+        n_layers: int=4,
         max_width: int=256
     ):
         """
